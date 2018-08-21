04299d2fb2dcead412001a1033156e116d505b19
anawolfmobot <42562455+anawolfmobot@users.noreply.github.com>
2018-08-21 05:55:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,21 +15,21 @@ if (message.content.startsWith(adminprefix + 'setgame')) {
 });
 
 client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setWatch")) {
-       if(message.author.id !== '303311682929688577') return;
-       client.user.setActivity(args,{type: 'WATCHING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setListen")) {
-       if(message.author.id !== '303311682929688577') return;
-       client.user.setActivity(args,{type: 'LISTENING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
+        let args = message.content.split(" ").slice(1).join(" ");
+       if(message.content.startsWith(adminprefix + "setWatch")) {
+           if(message.author.id !== '303311682929688577') return;
+           client.user.setActivity(args,{type: 'WATCHING'});
+           message.channel.send("**- :white_check_mark: Done!,**");
+       } 
+    });
+    client.on('message',function(message) {
+        let args = message.content.split(" ").slice(1).join(" ");
+       if(message.content.startsWith(adminprefix + "setListen")) {
+           if(message.author.id !== '303311682929688577') return;
+           client.user.setActivity(args,{type: 'LISTENING'});
+           message.channel.send("**- :white_check_mark: Done!,**");
+       } 
+    });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
