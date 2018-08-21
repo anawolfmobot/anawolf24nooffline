deabede186ff500aca913dcddb0a79b16a6a4fca
anawolfmobot <42562455+anawolfmobot@users.noreply.github.com>
2018-08-21 05:42:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ownerid = '303311682929688577';
+const adminprefix = "$";
+const devs = ['303311682929688577'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+}
 
-client.on('ready', () => {
-    console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message',function(message) {
+    let args = message.content.split(" ").slice(1).join(" ");
+   if(message.content.startsWith(adminprefix + "setWatch")) {
+       if(message.author.id !== '303311682929688577') return;
+       client.user.setActivity(args,{type: 'WATCHING'});
+       message.channel.send("**- :white_check_mark: Done!,**");
+   } 
+});
+client.on('message',function(message) {
+    let args = message.content.split(" ").slice(1).join(" ");
+   if(message.content.startsWith(adminprefix + "setListen")) {
+       if(message.author.id !== '303311682929688577') return;
+       client.user.setActivity(args,{type: 'LISTENING'});
+       message.channel.send("**- :white_check_mark: Done!,**");
+   } 
 });
 
 // THIS  MUST  BE  THIS  WAY
